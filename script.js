ce14a208ae59cb634039a0f3de5fba99c8eece3c
Ashley <ashleywu2016@u.northwestern.edu>
2014-12-12 15:07:39 -0600

    so much stuff

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 
+	 	console.log("damn u look good");
+
+//hover functions
+
+	//helmet
 	$("#helmet").hover(function(){
 		$("#helmet").attr("src", "images/helmetcolor.png");
 	});
@@ -8,6 +13,7 @@ $(document).ready(function(){
 		$("#helmet").attr("src", "images/helmet.png");
 	});
 
+	//ball
 	$("#ball").hover(function(){
 		$("#ball").attr("src", "images/footballcolor.png");
 	});
@@ -16,5 +22,69 @@ $(document).ready(function(){
 		$("#ball").attr("src", "images/football.png");
 	});
 
+	//pads
+	$("#pads").hover(function(){
+		$("#pads").attr("src", "images/footballcolor.png");
+	});
 
+	$("#pads").mouseleave(function(){
+		$("#pads").attr("src", "images/football.png");
+	});
+
+	//cleats
+	$("#cleats").hover(function(){
+		$("#cleats").attr("src", "images/helmetcolor.png");
+	});
+
+	$("#cleats").mouseleave(function(){
+		$("#cleats").attr("src", "images/helmet.png");
+	});
+
+	//when each icon is clicked:
+
+	$("#helmet").click(function(){	
+		//show helmet timeline and hide others no matter what
+		$("#helmet-timeline").slideToggle();
+		$("#ball-timeline, #pads-timeline, #cleats-timeline").hide();
+	});
+
+	$("#ball").click(function(){
+		$("#ball-timeline").slideToggle();
+		$("#helmet-timeline, #pads-timeline, #cleats-timeline").hide();
+	});
+
+	$("#pads").click(function(){	
+		//show helmet timeline and hide others no matter what
+		$("#pads-timeline").slideToggle();
+		$("#ball-timeline, #helmet-timeline, #cleats-timeline").hide();
+	});
+
+	$("#cleats").click(function(){
+		$("#cleats-timeline").slideToggle();
+		$("#helmet-timeline, #ball-timeline, #pads-timeline").hide();
+	});
+
+
+	$(".hide").click(function(){
+		$("#helmet-timeline, #ball-timeline, #pads-timeline, #cleats-timeline").slideUp();
+	});
+
+	//rumbling of headline
+
+	 $("#hero").jrumble({
+		speed:200
+	});
+		
+	$("#hero").hover(function(){
+		$("#hero").trigger("startRumble");
+	});
+
+	$("#hero").mouseleave(function(){
+		$("#hero").trigger("stopRumble");
+	});
+
+
+ 	var hero = $("#hero").length(); 
+
+			
 });
